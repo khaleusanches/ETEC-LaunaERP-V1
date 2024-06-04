f4f2b8f33c6844cea1b7136f8629831d63dcc9ac
claraaraujo2 <144171290+claraaraujo2@users.noreply.github.com>
2024-06-04 17:12:28 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,134 +1,93 @@
 create database projeto;
 use projeto;
-drop database projeto;
 
 --------------------------------------------
 
 create table fornecedores(
-id int auto_increment unique not null,
+id int auto_increment unique not null,    primary key(id),
 nome varchar(64) unique not null,
 cnpj varchar(18) unique not null,
 endereco varchar(200),
-email varchar(80) unique not null,
-primary key(id)
+email varchar(80) unique not null
 );
-
-select * from fornecedores;
-drop table fornecedores;
 
 
 create table telefones(
-id int unique not null auto_increment,
-idfornecedorfk int not null,
+id int unique not null auto_increment,    primary key (id),
+idfornecedorfk int not null,              foreign key (idfornecedorfk) references fornecedores (id),
 tel varchar(16) unique not null,
-obs varchar (48),
-primary key (id),
-foreign key (idfornecedorfk) references fornecedores (id)
+obs varchar (48)
 );
-
-select * from telefones;
-drop table telefones;
 
 --------------------------------------------
 
 create table produtos(
-id int unique not null auto_increment,
+id int unique not null auto_increment,    primary key(id)
 nome varchar(64) unique not null,
 valor decimal (10,2),
 estoque int,
-descricao varchar(180),
-primary key(id)
+descricao varchar(180)
 );
-
-select * from produtos;
-drop table produtos;
 
 ----------------------------
 
 create table lotes(
-id int unique not null auto_increment,
-idprodutofk int not null,
-quantidade int not null, check (quantidade>0),
+id int unique not null auto_increment,    primary key(id),
+idprodutofk int not null,                 foreign key (idprodutofk) references produtos (id),
+quantidade int not null,                  check (quantidade>0),
 fornecedor varchar(64) not null,
 aquisicao date not null,
 fabricacao date not null,
 validade date not null,
 notafiscal int unique not null,
-localizacao varchar(32) not null,
-primary key(id),
-foreign key (idprodutofk) references produtos (id)
+localizacao varchar(32) not null
 );
-
-select * from lotes;
-drop table lotes;
 
 --------------------------------------------
 
 create table operacoes(
-id int unique not null auto_increment,
-idclientefk int,
-total decimal(10,2),
-troco decimal(10,2),
+id int unique not null auto_increment,    primary key(id),
+idclientefk int,                          foreign key (idclientefk) references clientes (id),
+total decimal(10,2),                      check (total>0),
+troco decimal(10,2),                      check (troco>0),
 desconto decimal(10,2),
-dataehora datetime not null,
-primary key(id),
-foreign key (idclientefk) references clientes (id)
+dataehora datetime not null
 );
 
 
 create table vendas(
-id int unique not null auto_increment,
-idoperacaofk int not null,
-idprodutofk int,
-quantidade int,
-primary key (id),
-foreign key (idoperacaofk) references operacoes (id)
-foreign key (idprodutofk) references produtos (id),
+id int unique not null auto_increment,    primary key (id),
+idoperacaofk int not null,                foreign key (idoperacaofk) references operacoes (id),
+idprodutofk int not null,                 foreign key (idprodutofk) references produtos (id),
+quantidade int not null
 );
-
-select * from vendas;
-drop table vendas;
 
 --------------------------------------------
 
 create table clientes(
-id int unique not null auto_increment,
+id int unique not null auto_increment,    primary key(id),
 nome varchar(64) not null,
 cpf varchar(11) unique not null,
-desconto decimal(10,2) not null default '0.8',
-primary key(id)
+desconto decimal(10,2) not null default '0.8'
 );
-
-select * from clientes;
-drop table clientes;
 
 --------------------------------------------
 
 create table setores(
-id int unique not null auto_increment,
-nome varchar(32) unique not null,
-primary key(id)
+id int unique not null auto_increment,   primary key(id),
+nome varchar(32) unique not null
 );
-
-select * from setores;
-drop table setores;
-
 
 create table cargos(
-id int unique not null auto_increment,
+id int unique not null auto_increment,  primary key(id),
 nome varchar(32) unique not null,
-descricao varchar (180),
-nivel int not null,
-primary key(id)
+descricao varchar (180)
 );
-
-select * from cargos;
-drop table cargos;
 
 --------------------------------------------
 
 create table funcionarios(
-id int unique not null auto_increment,
+id int unique not null auto_increment,     primary key(id),
 nome varchar(64) not null,
 email varchar(64) unique not null,
 tel varchar(16) unique not null,
@@ -136,20 +95,14 @@ rg varchar(9) unique not null,
 nascimento date not null,
 pis varchar(11) unique not null,
 endereco varchar(200) not null,
-idcargofk int not null,
-idsetorfk int not null,
+idcargofk int not null,                    foreign key (idcargofk) references cargos (id),
+idsetorfk int not null,                    foreign key (idsetorfk) references setores (id),
 admissao date not null,
 salario decimal(10,2) not null,
 login varchar(32) unique not null,
 senha varchar(32) not null,
-desconto decimal(10,2) not null default '0.7',
-primary key(id),
-foreign key (idcargofk) references cargos (id),
-foreign key (idsetorfk) references setores (id)
+desconto decimal(10,2) not null default '0.7'
 );
-
-select * from funcionarios;
-drop table funcionarios;
 
 --------------------------------------------
 
