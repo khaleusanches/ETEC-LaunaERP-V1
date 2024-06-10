b5e6f6ba73860ff63efb78b48433857b35711846
Khaleu Mancini <khaleu.mancini@etec.sp.gov.br>
2024-06-10 17:56:42 -0300

    Refazendo

--- Telas/Telas/TelaLogin.Designer.cs
+++ Telas/Telas/TelaLogin.Designer.cs
@@ -30,16 +30,55 @@ namespace Telas
         /// </summary>
         private void InitializeComponent()
         {
-            LabelP lbUsername = new LabelP(250, 100, 0, 300, "Nome de Usuário:", this);
-            LabelP lbPassword = new LabelP(100, 100, 200, 300, "Senha:", this);
-            TextBoxP tbUsername = new TextBoxP(100, 100, 120, 300, "", 55, this);
-
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.Font = new System.Drawing.Font("Arial", 10F);
+            this.lbUsername.Location = new System.Drawing.Point(300, 0);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(250, 100);
+            this.lbUsername.TabIndex = 0;
+            this.lbUsername.Text = "Nome de Usuário:";
+            this.lbUsername.Click += new System.EventHandler(this.lbUsername_Click);
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.Font = new System.Drawing.Font("Arial", 10F);
+            this.lbPassword.Location = new System.Drawing.Point(300, 200);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(100, 100);
+            this.lbPassword.TabIndex = 1;
+            this.lbPassword.Text = "Senha:";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Font = new System.Drawing.Font("Arial", 10F);
+            this.tbUsername.Location = new System.Drawing.Point(300, 120);
+            this.tbUsername.MaxLength = 55;
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 23);
+            this.tbUsername.TabIndex = 2;
+            // 
+            // TelaLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbUsername);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Name = "TelaLogin";
             this.Text = "TelaLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private LabelP lbUsername;
+        private LabelP lbPassword;
+        private TextBoxP tbUsername;
     }
 }
--- Telas/Telas/TelaLogin.cs
+++ Telas/Telas/TelaLogin.cs
@@ -16,5 +16,9 @@ namespace Telas
         {
             InitializeComponent();
         }
+
+        private void lbUsername_Click(object sender, EventArgs e)
+        {
+        }
     }
 }
