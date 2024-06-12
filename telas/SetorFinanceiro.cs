604658c009f1560f72ee7ec014b371d97e9df429
claraaraujo2 <144171290+claraaraujo2@users.noreply.github.com>
2024-06-11 22:32:44 -0300

    Update SetorFinanceiro.cs

--- telas/SetorFinanceiro.cs
+++ telas/SetorFinanceiro.cs
@@ -10,16 +10,15 @@ using TL_Principal;
 using TL_Gerente;
 using TL_Estoque;
 
-namespace SetorRH'                '
+namespace SetorFinanceiro
 {
-    public class SetorRH : GerMercadorias
+    public class SetorFinanceiro : GerMercadorias
     {
         DataTable dt = new DataTable();
         Banco banco = new Banco();
         DataTable DT_consultaID = new DataTable();
 
-
-        
+        //--------------------------------------------------------------------
         public DataTable ExbFornecedores() 
         {
             dt = banco.consultar(
@@ -36,7 +35,77 @@ namespace SetorRH'                '
             +"order by fornecedores.id");
             return dt;
         }
+        //-----------------------------
+        public void CadFornecedor(string nome, string cnpj, string email, string endereco)
+        {
+          banco.comandar(
+            "insert into fornecedores(nome, cnpj, email, endereco) " +
+            "values ('"+nome+"','"+cnpj+"','"+email+"','"+endereco+"')");
+            fechar();
+        }
         
+        public void CadTel(int fornecedor, string tel, string obs)
+        {
+          banco.comandar(
+            "insert into fornecedores(idfornecedorfk, tel, obs) " +
+            "values ('"+fornecedor+"','"+tel+"','"+obs+"')");
+            fechar();
+        }
+        //-----------------------------      
+        public void DelFornecedor(int i)
+        {
+            banco.comandar("Delete * from telefones where idfornecedoresfk = '"+i+"'");
+            fechar();
+            banco.comandar("Delete * from fornecedores where id = '"+i+"'");
+            fechar();
+        }
+        
+        public void DelFornecedor(int i)
+        {
+            banco.comandar("Delete * from telefones where idfornecedoresfk = '"+i+"'");
+            fechar();
+        }
+        //-----------------------------
+        public void AtuFornecedor(string nome, string cnpj, string email, string endereco) 
+        {
+            banco.comandar(
+                "update fornecedores set"
+                    +" nome = '"+nome+"',"
+                    +" cnpj = '"+cnpj+"',"
+                    +" email = '"+email+"',"
+                    +" endereco = '"+endereco+"'"
+                +" where id = '"+i+"'");
+            fechar();
+        }
+        
+        public void AtuTel(int fornecedor, string tel, string obs)
+        {
+            banco.comandar(
+                "update fornecedores set"
+                    +" idfornecedorfk = '"+fornecedor+"',"
+                    +" tel = '"+tel+"',"
+                    +" obs = '"+obs+"'"
+                +" where id = '"+i+"'");
+            fechar();
+        }
+        //--------------------------------------------------------------------        
+        public DataTable ExbFuncionarios() 
+        {
+            dt = banco.consultar(
+            "select"
+                +" funcionarios.nome as 'Nome',"
+                +" id as 'ID', cargos.nome as 'Cargo',"
+                +" setores.nome as 'Setor', email as 'Email',"
+                +" tel as 'Telefone', pis as 'PIS',"
+                +" admissao as 'Data de admissão',"
+                +" salario as 'Salário',"
+                +" desconto as 'Desconto de Funcionário'"
+            +" from funcionarios"
+            +" inner join cargos on idcargofk = cargo.id"
+            +" inner join setores on idsetorfk = setores.id");
+            return dt;
+        }
+        //-----------------------------
         public void CadFuncionario(string nome, string cargo, string setor, string email, string tel, string pis, string admissao, string salario, string desconto)
         {
           banco.comandar(
@@ -44,13 +113,13 @@ namespace SetorRH'                '
             "values ('"+nome+"','"+cargo+"','"+setor+"','"+email+"','"tel+"','"+pis+"','"+admissao+"','"+salario,+"','"+desconto+"')");
             fechar();
         }
-        public void DelFuncionario(int i)
-        
+        //-----------------------------
+        public void DelFuncionario(int i)        
         {
             banco.comandar("Delete * from funcionarios where id = '"+i+"'");
             fechar();
         }
-        
+        //-----------------------------
         public void AtuFuncionario(int i, string nome, string cargo, string setor, string email, string tel, string pis, string admissao, string salario, string desconto) 
         {
             banco.comandar(
@@ -67,102 +136,7 @@ namespace SetorRH'                '
                 +" where id = '"+i+"'");
             fechar();
         }
-        
-        public DataTable ExbSetores() 
-        {
-            dt = banco.consultar(
-            "select	id as 'ID', nome as 'Nome' from setores");
-            return dt;
-        }
-        
-        public void CadSetor(string nome)
-        {
-          banco.comandar(
-            "insert into setores(nome) values ('"+nome+"')");
-            fechar();
-        }
-        
-        public void DelSetor(int i)
-        {
-          dt = banco.consultar(
-              "select setores.nome as 'Setor',"
-                +" cargos.nome as 'Cargo',"
-                +" nome as 'Nome',"
-                +" email as 'Email',"
-                +" tel as 'Telefone',"
-                +" pis as 'PIS',"
-                +" admissao as 'Data de admissão',"
-                +" salario as 'Salário',"
-                +" from funcionarios"
-            +" inner join cargos on idcargofk = cargo.id"
-            +" inner join setores on idsetorfk = '"+i+"'"
-          );
-          if (dt != null) {
-            Console.WriteLine("Este setor não pode ser deletado. Remova os funcionários pertencentes à este setor.");
-          }
-          else {
-            banco.comandar("Delete * from setores where id = '"+i+"'");
-            fechar();
-          }
-        }
-        
-        public void AtlSetor(int id, string nome) 
-        {
-            banco.comandar(
-                "update setores set"
-                    +" id = '"+id+"',"
-                    +" nome = '"+nome+"'");
-            fechar();
-        }
-
-        
-        public DataTable ExbCargos() 
-        {
-            dt = banco.consultar(
-            "select	id as 'ID', nome as 'Nome' from cargos");
-            return dt;
-        }
-        
-        public void CadCargo(string nome)
-        {
-          banco.comandar(
-            "insert into cargos(nome) values ('"+nome+"')");
-            fechar();
-        }
-        
-        public void DelCargo(int i)
-        {
-          dt = banco.consultar(
-              "select cargos.nome as 'Cargo',"
-                  +" setores.nome as 'Setor',"
-                  +" nome as 'Nome',"
-                  +" email as 'Email',"
-                  +" tel as 'Telefone',"
-                  +" pis as 'PIS',"
-                  +" admissao as 'Data de admissão',"
-                  +" salario as 'Salário',"
-                  +" from funcionarios"
-              +" inner join cargos on idcargofk = cargo.id"
-              +" inner join setores on idsetorfk = '"+i+"'");
-          if (dt != null) {
-            Console.WriteLine("Este cargo não pode ser deletado. Remova os funcionários pertencenter à este cargo.");
-          }
-          else {
-            banco.comandar("Delete * from cargps where id = '"+i+"'");
-            fechar();
-          }
-        }
-        
-        public void AtlCargo(int id, string nome, string descricap) 
-        {
-            banco.comandar(
-                "update cargos set"
-                    +" id = '"+id+"',"
-                    +" nome = '"+nome+"',"
-                    +" descricao = '"+descricao+"'");
-            fechar();
-        }
-                           
+        //--------------------------------------------------------------------                    
                          
         public void fechar()
         {
