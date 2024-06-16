eb3c6a2f6566909ac03f58d81f79d64b5b1b34bf
claraaraujo2 <144171290+claraaraujo2@users.noreply.github.com>
2024-06-16 15:59:04 -0300

    Create GerFinanceiro.cs

---
+++ telas/GerFinanceiro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BancoDados;
+using TL_Principal;
+using TL_Gerente;
+using TL_Estoque;
+
+namespace GerFinanceiro
+{
+    public class GerFinanceiro : SetorFinanceiro
+    {
+        DataTable dt = new DataTable();
+        Banco banco = new Banco();
+        DataTable DT_consultaID = new DataTable();
+
+        //--------------------------------------------------------------------
+        public DataTable ExbOperacoesCaixaBasico() 
+        //recentes para antigas
+        {
+            dt = banco.consultar(
+            "select"
+                +" id as 'ID',"
+                +" idclientefk as 'Cliente',"
+                +" total as 'Total',"
+                +" troco as 'Troco',"
+                +" dataehora as 'Data e hora'"
+                +" idfuncionariofk as 'Operador de caixa',"
+            +" from operacoes order by dataehora desc");
+            return dt;
+        }
+        //------------------------------------------
+                         
+        public void fechar()
+        {
+            dt.Clear();
+        }
+    }
+}
