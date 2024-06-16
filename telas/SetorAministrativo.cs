2f55fbc5e209fbd6f337d0cb11a5d81b87291aca
claraaraujo2 <144171290+claraaraujo2@users.noreply.github.com>
2024-06-16 19:40:33 -0300

    Create SetorAministrativo.cs

---
+++ telas/SetorAministrativo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BancoDados;
+using TL_Principal;
+using TL_Gerente;
+using TL_Estoque;
+
+namespace SetorAministrativo
+{
+    public class SetorAministrativo : SetorFinanceiro
+    {
+        DataTable dt = new DataTable();
+        Banco banco = new Banco();
+        DataTable DT_consultaID = new DataTable();
+
+        //--------------------------------------------------------------------                    
+                         
+        public void fechar()
+        {
+            dt.Clear();
+        }
+    }
+}
